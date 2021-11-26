0337b657267ea147247aa199bde2c738731048fe
Syeda Sana Zehra <sana.zehra@tpsonline.com>
2021-11-26 14:29:59 +0500

    improved logging, added a collector config

--- tracing.js
+++ tracing.js
@@ -7,17 +7,15 @@ const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 
+//Alternative: Use OTLP collector to configure certain sets of exporters, processors and receivers
 const collectorOptions = {
-  // url: 'https://my-deployment-97901c.apm.us-central1.gcp.cloud.es.io', // url is optional and can be omitted - default is http://localhost:55681/v1/traces
-  // headers: {}, // an optional object containing custom headers to be sent with each request
-  // concurrencyLimit: 10, // an optional limit on pending requests
+    // url: 'https://my-deployment-97901c.apm.us-central1.gcp.cloud.es.io', // url is optional and can be omitted - default is http://localhost:55681/v1/traces
+    // headers: {}, // an optional object containing custom headers to be sent with each request
+    // concurrencyLimit: 10, // an optional limit on pending requests
 };
-
 const exporter1 = new OTLPTraceExporter(collectorOptions);
-//console.log(exporter1.getDefaultUrl());
 
-
- function start(appName) {
+function start(appName) {
     var exporter = new JaegerExporter();
     const sdk = new opentelemetry.NodeSDK({
         resource: new Resource({
@@ -25,9 +23,9 @@ const exporter1 = new OTLPTraceExporter(collectorOptions);
         }),
         instrumentations: [getNodeAutoInstrumentations()],
         traceExporter: exporter,
-       spanProcessor: new SimpleSpanProcessor(exporter),
+        spanProcessor: new SimpleSpanProcessor(exporter),
     });
     sdk.start();
 }
 
-module.exports = {start}
+module.exports = { start }
