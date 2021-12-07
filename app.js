003359a04f417a447e7b51f772200d0346f2f2e0
Syeda Sana Zehra <sana.zehra@tpsonline.com>
2021-12-08 02:24:30 +0500

    added metrics for reference

--- app.js
+++ app.js
@@ -2,10 +2,39 @@ const express = require("express");
 //import tracing reference
 const trace = require('./tracing');
 trace.start("SAMPLE-SERVICE");
+//import monitoring
+const appMonitor = require("./monitoring");
+appMonitor.init("SAMPLE-SERVICE");
 const PORT = process.env.PORT || "8080";
 const app = express();
 const { work } = require('./work');
 var ip = require("ip");
+
+let monitoringClient = appMonitor.getClient();
+
+
+const httpRequestDurationSeconds = new monitoringClient.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10] // 0.1 to 10 seconds
+});
+const errorsTotal = new monitoringClient.Counter({
+  name: 'errors_total',
+  help: 'Total number of ERRORS in route',
+  labelNames: ['api_error_count', 'route']
+});
+
+
+const routesTotal = new monitoringClient.Counter({
+  name: 'request_count_total',
+  help: 'Total number of requests on server',
+  labelNames: ['route']
+});
+
+appMonitor.registerMetric(httpRequestDurationSeconds);
+appMonitor.registerMetric(errorsTotal);
+appMonitor.registerMetric(routesTotal);
 
 
 
@@ -15,13 +44,13 @@ var ip = require("ip");
 //   // Override the service name from package.json
 //   // Allowed characters: a-z, A-Z, 0-9, -, _, and space
 //   serviceName: 'new-service',
-  
+
 //   // Use if APM Server requires a secret token
 //   secretToken: 'pHEIKVYnyFpe9Rq1kQ',
-  
+
 //   // Set the custom APM Server URL (default: http://localhost:8200)
 //   serverUrl: 'https://my-deployment-97901c.apm.us-central1.gcp.cloud.es.io',
-  
+
 //   // Set the service environment
 //   environment: 'production'
 //   })
@@ -32,36 +61,62 @@ app.get("/", (req, res) => {
 
 
 app.get("/work", async (req, res) => {
-
+  var stopCallBack = httpRequestDurationSeconds.startTimer();
+  let url = req.originalUrl.split("/").pop();
+  routesTotal.inc({
+    route: `/${url}`
+  });
   // refer this to implement tracing using span.js
-    await work(req);
-    var json = {
-      "status": "OK",
-      "timeStamp": new Date().toISOString()
-    }
-    res.header("Content-Type", "application/json")
-    res.send(json);
+  await work(req);
+  var json = {
+    "status": "OK",
+    "timeStamp": new Date().toISOString()
+  }
+  res.header("Content-Type", "application/json")
+  res.send(json);
+  stopCallBack({route:url,method:req.method,code:200});
 
 });
 
 app.get("/metrics", async (req, res) => {
+  let url = req.originalUrl.split("/").pop();
+  routesTotal.inc({
+    route: `/${url}`
+  });
   // Return all metrics the Prometheus exposition format
-  res.setHeader('Content-Type', register.contentType)
+  // res.setHeader('Content-Type', register.contentType)
+  // var metrics = await register.metrics();
+  // res.end(metrics);
+
+  let register = appMonitor.getRegistry();
+  res.setHeader('Content-Type', register.contentType);
   var metrics = await register.metrics();
   res.end(metrics);
+
+
 });
 
 app.get("/health", async (req, res) => {
+  var stopCallBack = httpRequestDurationSeconds.startTimer();
 
+  let url = req.originalUrl.split("/").pop();
+  routesTotal.inc({
+    route: `/${url}`
+  });
   // return an error 1% of the time
   var num = Math.floor(Math.random() * 100);
-  if ((num)%2==0) {
+  if ((num) % 2 == 0) {
     //throw new Error('Internal Error');
     var json = {
       "status": "NOT OK",
       "timeStamp": new Date().toISOString(),
       "num": num
     }
+    errorsTotal.inc({
+      api_error_count: `/${url} Errors`,
+      route: `/${url}`
+    });
+    stopCallBack({route:url,method:req.method,code:500});
     res.header("Content-Type", "application/json");
     res.status(500).send(json);
   }
@@ -72,6 +127,7 @@ app.get("/health", async (req, res) => {
       "timeStamp": new Date().toISOString(),
       "num": num
     }
+    stopCallBack({route:url,method:req.method,code:200});
     res.header("Content-Type", "application/json")
     res.send(json);
   }
--- monitoring.js
+++ monitoring.js
@@ -1,82 +1,30 @@
-/* monitoring.js */
-'use strict';
+const promClient = require('prom-client');
+let register = new promClient.Registry();
 
-const { MeterProvider, ConsoleMetricExporter } = require('@opentelemetry/metrics');
-const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
-const client = require('prom-client');
-const collectDefaultMetrics = client.collectDefaultMetrics;
-const Registry = client.Registry;
-const register = new Registry();
-module.exports.register = register;
-collectDefaultMetrics({ register });
-
-
-const meter = new MeterProvider({
-  exporter: new PrometheusExporter(
-    {
-      startServer: true,
-      port: 8081
-    },
-    () => {
-      console.log("prometheus scrape endpoint: http://localhost:"
-        + "8081"
-        + "/metrics");
-    }
-  )
-}).getMeter('prometheus');
-
-const requestCount = meter.createCounter("requests", {
-  description: "Count all incoming requests"
-});
-
-
-const tester = meter.createValueRecorder('tester', {
-  description: 'test span of req processing',
-  boundaries: [0.1, 0.2, 0.3, 0.5, 1, 1.1, 1.2, 1.8, 1.9]
-});
-
-
-var histogramSpan = new client.Histogram({
-  name: 'histo_tester_prom',
-  help: 'histo_tester_prom',
-  buckets: [0.1, 0.2, 0.3, 0.5, 1, 1.1, 1.2, 1.8, 1.9],
-});
-
-const errorsTotal = new client.Counter({
-  name: 'errors_total',
-  help: 'Total number of ERRORS in route',
-  labelNames: ['api_error_count']
-});
-
-
-register.registerMetric(errorsTotal);
-register.registerMetric(histogramSpan);
-
-
-const boundInstruments = new Map();
-
-module.exports.countAllRequests = () => {
-  return (req, res, next) => {
-    if (!boundInstruments.has(req.path)) {
-      const labels = { route: req.path };
-      const boundCounter = requestCount.bind(labels);
-      boundInstruments.set(req.path, boundCounter);
+class Monitor {
+    constructor() {
     }
 
-    boundInstruments.get(req.path).add(1);
-    next();
-  };
-};
+    init(appName) {
+        register.setDefaultLabels({
+            app: appName
+        });
+        promClient.collectDefaultMetrics({ register });
+    }
 
+    getRegistry() {
+        return register;
+    }
 
-module.exports.incrementReqInErrorCount = () => {
-  errorsTotal.inc({
-    api_error_count: "Health Errors"
-  });
+    getClient() {
+        return promClient;
+    }
+
+    registerMetric(metric) {
+        register.registerMetric(metric);
+    }
+
 }
 
-module.exports.recordSpan = (value) => {
-  console.log('recording span ' + value)
-  tester.record(value);
-  histogramSpan.observe(value);
-}
+var Monitor_ = new Monitor();
+module.exports = Monitor_;
